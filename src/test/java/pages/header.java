1fc149c958f4a1fe59d1fa816b754c6dcbda281e
Enes <enes.buke12@gmail.com>
2023-12-13 22:11:21 +0300

    First commit for Project

--- src/test/java/pages/header.java
+++ src/test/java/pages/header.java
@@ -1,146 +1,56 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.BaseDriver;
 
 public class header {
 
-    public header()
-    {
+    public header() {
         PageFactory.initElements(BaseDriver.getDriver(), this);
     }
 
-    //////////////////////Aygül/////////////////////
-
-
-
-
-
-    ///////////////////Senol/////////////////////////
-
-
-
-
-
-
-    //////////////////////Musab///////////////////////
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Göksah///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Enes///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Talat///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Ümit///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Talip///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Sokol///////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //////////////////////Rustam///////////////////////
-
-
-
-
-
-
-
-
-
-
-    public WebElement getWebelement(String stringElement)
-    {switch (stringElement)
-    {
-
-
-
-
-    }
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[1]")
+    public WebElement coursesButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[2]")
+    public WebElement calendarButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[3]")
+    public WebElement attendanceButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[4]")
+    public WebElement assignmentsButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[5]")
+    public WebElement gradingButton;
+    @FindBy(xpath = "//button[@class='mat-mdc-menu-trigger mat-button-wrapper mdc-button mat-mdc-button mat-unthemed mat-mdc-button-base']")
+    public WebElement hamburgerMenu;
+    @FindBy(xpath = "//span[text()='Messaging']")
+    public WebElement messagingButton;
+    @FindBy(xpath = "//span[text()='New Message']")
+    public WebElement newMessageButton;
+    @FindBy(xpath = "//span[text()='Inbox']")
+    public WebElement inboxButton;
+    @FindBy(xpath = "//span[text()='Outbox']")
+    public WebElement outboxButton;
+    @FindBy(xpath = "//span[text()='Trash']")
+    public WebElement trashButton;
+    @FindBy(xpath = "//span[text()='Finance']")
+    public WebElement financeButton;
+    @FindBy(xpath = "//span[text()='My Finance']")
+    public WebElement myFinanceButton;
+    @FindBy(xpath = "//button[@class='mat-mdc-menu-trigger user-button mdc-button mat-mdc-button mat-unthemed mat-mdc-button-base ng-star-inserted']")
+    public WebElement profileButton;
+    @FindBy(xpath = "//span[text()='Settings']")
+    public WebElement settingsButton;
+
+
+    public WebElement getWebelement(String stringElement) {
+        switch (stringElement) {
+
+
+        }
 
         return null;
     }
-
 
 
 }
