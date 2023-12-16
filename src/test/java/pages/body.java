f7f697a64dcd3f0cc10dbcad169abb10db2ad3fc
Talat <javaucun@gmail.com>
2023-12-16 11:15:44 +0300

    US003 bittirdim

--- src/test/java/pages/body.java
+++ src/test/java/pages/body.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.BaseDriver;
 
+import java.util.List;
+
 public class body extends Methods{
 
     public body()
@@ -93,11 +95,99 @@ public class body extends Methods{
 
 
     //////////////////////Talat///////////////////////
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[1]")
+    public WebElement coursesButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[2]")
+    public WebElement calendarButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[3]")
+    public WebElement attendanceButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[4]")
+    public WebElement assignmentsButton;
+    @FindBy(xpath = "(//ms-layout-menu-button//button)[5]")
+    public WebElement gradingButton;
+    @FindBy(xpath = "//button[@class='mat-mdc-menu-trigger mat-button-wrapper mdc-button mat-mdc-button mat-unthemed mat-mdc-button-base']")
+    public WebElement hamburgerMenu;
+    @FindBy(xpath = "//span[text()='Messaging']")
+    public WebElement messagingButton;
+    @FindBy(xpath = "(//div/span[1])[6]")
+    public WebElement welcomeText;
 
+    @FindBy(xpath = "//span[contains(.,' Calendar ')]")
+    public WebElement calendarText;
+    @FindBy(xpath = "//span[contains(.,' Attendance ')]")
+    public WebElement attendanceText;
+    @FindBy(xpath = "//span[contains(.,' Assignments ')]")
+    public WebElement assigmentsText;
+    @FindBy(xpath = "//span[contains(.,' Grading ')]")
+    public WebElement gradingText;
+    @FindBy(xpath = "//span[@class='mat-mdc-menu-item-text']")
+    public List<WebElement> hamburgMenuList;
+    @FindBy(xpath = "//span[contains(.,' Student_5 11A - Mark Zuckerberg')]")
+    public WebElement student_5Text;
+    @FindBy(xpath = "//span[contains(.,' Messages')]")
+    public WebElement messagesText;
+    @FindBy(xpath = "//div/user-chat-bell")
+    public WebElement chatsButton;
+    @FindBy(xpath = "//div/user-message-bell")
+    public WebElement messageBoxButton;
+    @FindBy(xpath = "//span[contains(.,' Student_5 11A - Mark Zuckerberg')]")
+    public WebElement chatsWindow;
+    @FindBy(xpath = "(//button[@aria-haspopup='menu'])[2]")
+    public WebElement profileButton;
 
+    @FindBy(xpath = "//button[@role='menuitem']")
+    public List<WebElement> profileButtonList;
 
+    public WebElement getWebelement(String stringElement) {
+        switch (stringElement) {
+            case "coursesButton":
+                return this.coursesButton;
+            case "calendarButton":
+                return this.calendarButton;
+            case "attendanceButton":
+                return this.attendanceButton;
+            case "assignmentsButton":
+                return this.assignmentsButton;
+            case "gradingButton":
+                return this.gradingButton;
+            case "hamburgerMenu":
+                return this.hamburgerMenu;
+            case "messagingButton":
+                return this.messagingButton;
+            case "welcomeText":
+                return this.welcomeText;
+            case "calendarText":
+                return this.calendarText;
+            case "attendanceText":
+                return this.attendanceText;
+            case "assigmentsText":
+                return this.assigmentsText;
+            case "gradingText":
+                return this.gradingText;
+            case "student_5Text":
+                return this.student_5Text;
+            case "messagesText":
+                return this.messagesText;
+            case "chatsButton":
+                return this.chatsButton;
+            case "messageBoxButton":
+                return this.messageBoxButton;
+            case "chatsWindow":
+                return this.chatsWindow;
+            case "profileButton":
+                return this.profileButton;
+        }
 
-
+        return null;
+    }
+    public List<WebElement> getWebelmentList(String stringList) {
+        switch (stringList) {
+            case "hamburgMenuList":
+                return this.hamburgMenuList;
+            case"profileButtonList": return this.profileButtonList;
+        }
+        return null;
+    }
 
 
 
@@ -202,15 +292,4 @@ public class body extends Methods{
 
 
 
-    public WebElement getWebelement(String stringElement)
-    {switch (stringElement)
-    {
-
-
-
-
-    }
-
-        return null;
-    }
 }
